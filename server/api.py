a17ce0012a9a4bbecdea39cd8c6df9dc88167e73
koayon <58336584+koayon@users.noreply.github.com>
2023-05-31 23:38:23 +0100

    Refactor

--- server/api.py
+++ server/api.py
@@ -1,43 +1,49 @@
 """
-The main API. It takes in a percentage and returns
+The get_billboard_data API. It takes in a percentage and returns
 the song that was number 1 on the Billboard Hot 100 on that day.
 """
 
 import datetime
 import json
-from typing import Tuple, Union, Optional
-from urllib.error import HTTPError
-import logging
-import os
+from typing import Tuple, Union
 from dataclasses import dataclass
-
-# from dotenv import load_dotenv
+from urllib.error import HTTPError
 from urllib.parse import unquote
 
 import billboard
 from fastapi import FastAPI, HTTPException, Request
-from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
-import lyricsgenius
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from env_vars import CLIENT_ID, CLIENT_SECRET
 
+SCOPE = "user-modify-playback-state user-read-currently-playing user-read-recently-played user-read-playback-state"
+PROD = False
+
+if PROD:
+    SPOTIFY_REDIRECT_URI = "https://trainingsong-1-h1171059.deta.app/api_callback"
+else:
+    SPOTIFY_REDIRECT_URI = "http://localhost:8000/api_callback"
+
 
 @dataclass
 class Song:
+    """A dataclass to store the song name, artist name and number of weeks on the chart"""
+
     artist: str
     weeks: int
     title: str
 
 
-def main(
+def get_billboard_data(
     percentage: float,
     chart: str = "hot-100",
-) -> Union[Tuple[str, str], None]:
+) -> Tuple[str, str, str]:
+    """Call Billboard API and get the song name, artist name and song info"""
     if percentage > 100 or percentage < 0:
-        print("Please enter a percentage between 0 and 100")
-        return
+        raise HTTPException(
+            status_code=400, detail="Please enter a percentage between 0 and 100"
+        )
     if percentage < 1:
         # Turn a decimal into a percentage
         percentage *= 100
@@ -47,57 +53,22 @@ def main(
     song_name = number_one_song.title
     artist_name = number_one_song.artist
 
-    print(
-        f"The song that was Number 1 {percentage}% through the 1900s was {song_name} by {artist_name}."
-    )
-    print(
-        f"The date was {target_date} and the song was on the chart for {number_one_song.weeks} weeks."
-    )
-    print(datetime.datetime.now())
-
-    # return percentage, song_name, artist_name, autoplay, target_date
-
-    # try:
-    #     sp = authenticate_spotify(spotify_client_id, spotify_client_secret)
-    #     # return True
-    # except HTTPError as e:
-    #     logging.debug(e)
-    #     print(
-    #         "Please ensure that Spotify is open on your device and you are logged in."
-    #     )
-    #     print("It may help to play a song on Spotify before trying again.")
-    #     raise HTTPException(
-    #         status_code=500, detail="Spotify authentication failed"
-    #     ) from e
-
-    # # devices = sp.devices()
-    # # device_list = devices['devices']
-
-    print("about to call spotify_link")
-    return song_name, artist_name
+    song_info = f"""The Number 1 song {percentage}% through the 1900s on the {chart} chart was {song_name} by {artist_name}. \n The date was {target_date} and the song was on the chart for {number_one_song.weeks} weeks."""
+    return song_name, artist_name, song_info
 
 
-def post_auth(sp, song_name, artist_name, autoplay) -> None:
-    link, name, uri = spotify_link(sp, song_name, artist_name)
-
-    print(f"Here's a link to the song {name} on Spotify")
-    print(link)
-    if autoplay:
-        try:
-            start_playing_on_spotify(sp, uri)
-        except HTTPError as e:
-            raise HTTPException(
-                status_code=424, detail="Spotify playback failed"
-            ) from e
-
-    # TODO: Throw print statements into function calls to clean this up
-    # TODO: Spotify describes the song as belonging to these genres:
-    # TODO: Genius song description:
+def start_playback(sp, uri, device_id=None) -> None:
+    """Start playing the song on Spotify"""
+    try:
+        sp.start_playback(device_id=device_id, uris=[uri])
+    except HTTPError as e:
+        raise HTTPException(status_code=424, detail="Spotify playback failed") from e
 
 
 def get_number_one_song(
     percentage: float, chart: str = "hot-100"
 ) -> Tuple[Song, datetime.date]:
+    """Get the number one song on the chosen Billboard chart on date that is percentage% through the 1900s"""
     target_year = int(percentage)
 
     # Calculate the target date based on the fractional part of the percentage
@@ -126,13 +97,7 @@ def get_number_one_song(
     # Maybe before that date I'll hardcode in a 5 songs for each year and just return a random one?
     # Can have some Easter eggs like Taylor's 22 etc.
 
-    # Maybe add in a random chart mode as well?
-
     return number_one_song, target_date
-
-
-def genius_description():
-    raise NotImplementedError
 
 
 def spotify_link(sp, song_name: str, artist_name: str) -> Tuple[str, str, str]:
@@ -147,52 +112,90 @@ def spotify_link(sp, song_name: str, artist_name: str) -> Tuple[str, str, str]:
     return link, name, uri
 
 
-def start_playing_on_spotify(sp, uri, device_id=None):
-    """Start playing song"""
-    sp.start_playback(device_id=device_id, uris=[uri])
-
-
 app = FastAPI()
-
-# load_dotenv()  # take environment variables from .env.
-
-# sp_client_id = os.getenv("CLIENT_ID")
-# sp_client_secret = os.getenv("CLIENT_SECRET")
-SPOTIFY_REDIRECT_URI = "http://localhost:8000/api_callback"
-# SPOTIFY_REDIRECT_URI = "https://trainingsong-1-h1171059.deta.app/api_callback"
-SCOPE = "user-modify-playback-state user-read-currently-playing user-read-recently-played user-read-playback-state"
 
 
 @app.get("/")
 async def root(
-    p: Union[float, None] = None, chart: str = "hot-100", autoplay: bool = False
+    p: Union[float, None] = None,
+    chart: str = "hot-100",
+    autoplay: bool = False,
 ):
+    """The main API endpoint. It takes in a percentage p, interacts with the billboard api and then redirects to the callback for the Spotify API."""
     if p is None:
         return {"Hello": "World"}
     try:
-        song_artist = main(p, chart)
-        if song_artist:
-            song_name, artist_name = song_artist
+        song_results = get_billboard_data(p, chart)
+        if song_results:
+            song_name, artist_name, song_info = song_results
         else:
             raise HTTPException(status_code=400, detail="No song found")
 
-        response = authenticate_spotify(song_name, artist_name, autoplay)
-        print("authenticated spotify")
+        response = authenticate_spotify(song_name, artist_name, autoplay, song_info)
         return response
 
-    # return {
-    #     "percentage": p,
-    #     "chart": chart,
-    #     "autoplay": autoplay,
-    #     "song_name": song_name,
-    #     "artist_name": artist_name,
-    # }
-
     except Exception as e:
-        return {"error": "Yo", "exception": str(e)}
+        raise HTTPException(status_code=404, detail=str(e)) from e
 
 
-def authenticate_spotify(song_name, artist_name, autoplay):
+@app.get("/api_callback")
+def callback(request: Request):
+    """The callback for the Spotify API once we've authenticated. Gets the song link and plays it if autoplay was selected. Then returns the song info to the endpoint."""
+    spotify_client_code = request.query_params.get("code")
+    state_data = request.query_params.get("state")
+    try:
+        song_name, artist_name, autoplay, song_info = parse_state_data(state_data)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e)) from e
+
+    try:
+        sp = create_spotify_client(spotify_client_code)
+    except HTTPError as e:
+        raise HTTPException(
+            status_code=400, detail="Spotify client authentication failed"
+        ) from e
+
+    link, _name, uri = spotify_link(sp, song_name, artist_name)
+    errors = None
+
+    if autoplay:
+        try:
+            start_playback(sp, uri)
+
+        except Exception:
+            errors = "Unable to start playback. Please ensure that Spotify is active on one of your devices and try again."
+
+    return {
+        "spotify_link": link,
+        "song_info": song_info,
+        "song_name": song_name,
+        "artist_name": artist_name,
+        #  "target_date": target_date
+        #  "percentage": percentage,
+        # "chart": chart
+        "errors": errors,
+    }
+
+
+def parse_state_data(state):
+    """Parse the state data from the Spotify API callback"""
+    if state:
+        state = unquote(state)
+        state = state.replace("+", " ")
+        state_data = json.loads(state)
+    else:
+        raise HTTPException(status_code=500, detail="Unable to parse state data")
+
+    song_name = state_data.get("song_name")
+    artist_name = state_data.get("artist_name")
+    autoplay = state_data.get("autoplay")
+    song_info = state_data.get("song_info")
+    return song_name, artist_name, autoplay, song_info
+
+
+def authenticate_spotify(song_name, artist_name, autoplay, song_info):
+    """Get the Spotify authentication URL"""
+
     # Create a SpotifyOAuth object
     sp_oauth = SpotifyOAuth(
         client_id=CLIENT_ID,
@@ -205,14 +208,15 @@ def authenticate_spotify(song_name, artist_name, autoplay):
         "song_name": song_name,
         "artist_name": artist_name,
         "autoplay": autoplay,
+        "song_info": song_info,
     }
-    # print(spotify_client_id == os.getenv("CLIENT_ID"))
     auth_url = sp_oauth.get_authorize_url(state=json.dumps(state_data))
-    print("auth url", auth_url)
     return RedirectResponse(auth_url)
 
 
 def create_spotify_client(code):
+    """Create a Spotify client using the code from the Spotify API callback"""
+
     sp_oauth = SpotifyOAuth(
         client_id=CLIENT_ID,
         client_secret=CLIENT_SECRET,
@@ -220,55 +224,10 @@ def create_spotify_client(code):
         scope=SCOPE,
     )
 
-    print("About to get access token")
     # Get the access token
-    # token_info = sp_oauth.get_cached_token() or sp_oauth.get_access_token()
     token_info = sp_oauth.get_access_token(code)
-    print("Got access token")
-    print("token info", token_info)
-
-    if token_info:
-        access_token = token_info["access_token"]
-    else:
-        raise HTTPException(status_code=500, detail="Unable to get access token")
+    access_token = token_info["access_token"] if token_info else None
 
     # Create a Spotify client with the access token
     sp = spotipy.Spotify(auth=access_token)
     return sp
-
-
-@app.get("/api_callback")
-def callback(request: Request):
-    print("Hit callback endpoint")
-    code = request.query_params.get("code")
-    state = request.query_params.get("state")
-
-    print(code)
-    print(state)
-
-    if state:
-        state = unquote(state)
-        state = state.replace("'", '"')
-        state = state.replace("+", " ")
-        state_data = json.loads(state)
-    else:
-        raise HTTPException(status_code=500, detail="Unable to parse state data")
-
-    song_name = state_data.get("song_name")
-    artist_name = state_data.get("artist_name")
-    autoplay = state_data.get("autoplay")
-
-    try:
-        sp = create_spotify_client(code)
-    except HTTPError as e:
-        print(
-            "Please ensure that Spotify is open on your device and you are logged in."
-        )
-        print("It may help to play a song on Spotify before trying again.")
-        raise HTTPException(
-            status_code=500, detail="Spotify authentication failed"
-        ) from e
-    try:
-        post_auth(sp, song_name, artist_name, autoplay)
-    except Exception as e:
-        raise HTTPException(status_code=424, detail=str(e)) from e
