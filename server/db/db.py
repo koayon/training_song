105006632147d45171ed6ef74da1761ce9d134ce
koayon <58336584+koayon@users.noreply.github.com>
2023-06-11 13:34:30 +0100

    Add db crud operations

--- server/db/db.py
+++ server/db/db.py
@@ -1,6 +1,6 @@
 import databases
 import sqlalchemy
-from sqlalchemy import Table, Column, String, text
+from sqlalchemy import Table, Column, String, text, Integer
 import os
 from dotenv import load_dotenv
 from typing import Optional, Dict
@@ -21,7 +21,7 @@ tokens = Table(
         Column("email", String, primary_key=True, unique=True),
         Column("access_token", String),
         Column("refresh_token", String),
-        Column("expires_at", String),
+        Column("expires_at", Integer),
     )
 
 engine = sqlalchemy.create_engine(
@@ -33,7 +33,7 @@ def create() -> None:
         connection.execute(text("DROP TABLE IF EXISTS tokens"))
     metadata.create_all(engine)
 
-async def store_tokens(email: str, access_token: str, refresh_token: str, expires_at: str) -> None:
+async def store_tokens(email: str, access_token: str, refresh_token: str, expires_at: int) -> None:
     query = tokens.insert().values(email=email, access_token=access_token, refresh_token=refresh_token, expires_at=expires_at)
     await database.execute(query)
 
@@ -42,6 +42,14 @@ async def get_tokens(email: str) -> Optional[Dict[str, str]]:
     query = tokens.select().where(tokens.c.email == email)
     result = await database.fetch_one(query)
     return result
+
+async def delete_tokens(email: str) -> None:
+    query = tokens.delete().where(tokens.c.email == email)
+    await database.execute(query)
+
+async def update_tokens(email: str, access_token: str, refresh_token: str, expires_at: str) -> None:
+    query = tokens.update().where(tokens.c.email == email).values(access_token=access_token, refresh_token=refresh_token, expires_at=expires_at)
+    await database.execute(query)
 
 @asynccontextmanager
 async def database_session():
@@ -54,8 +62,9 @@ async def database_session():
 async def main():
     async with database_session():
         # Test store_tokens and get_tokens
+        # create()
 
-        # await store_tokens("test", "test", "test", "test")
+        # await store_tokens("test", "test", "test", 1689727126)
         record = await get_tokens("test")
         if record:
             print(record)
