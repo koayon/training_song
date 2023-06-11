651a4db7171c22e03b0adb231bf2e9ef43243a9e
koayon <58336584+koayon@users.noreply.github.com>
2023-06-11 15:46:13 +0100

    Add training_song initial tests

---
+++ tests/test_training_song.py
@@ -0,0 +1,39 @@
+import pytest
+from fastapi.testclient import TestClient
+import responses
+
+
+def test_test():
+    assert 1 == 1
+
+
+from training_song.training_song import local_app, _training_song, is_valid_email
+
+client = TestClient(local_app)
+
+
+def test_is_valid_email():
+    assert is_valid_email("user@example.com")
+    assert not is_valid_email("user_at_example_dot_com")
+
+
+@responses.activate
+def test_training_song():
+    responses.add(
+        responses.GET,
+        "https://training-song-api-koayon.vercel.app",
+        json={"song_info": "mock song info"},
+        status=200,
+    )
+
+    acc, response = _training_song(0.9, verbose=False)
+
+    assert acc == 0.9
+    assert "song_info" in response
+    assert response["song_info"] == "mock song info"
+
+
+def test_spotify_callback():
+    response = client.get("/local_callback", params={"code": "12345"})
+    assert response.status_code == 200
+    assert response.text == '"Success! You can close this window."'
