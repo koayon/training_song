e0b1e3b9a971489eb0eb128cd76cd4a20337d479
koayon <58336584+koayon@users.noreply.github.com>
2023-06-11 18:31:00 +0100

    Add db tests

---
+++ tests/server/test_db.py
@@ -0,0 +1,45 @@
+import json
+import responses
+
+import pytest
+from unittest.mock import AsyncMock, MagicMock
+
+from training_song.server.db.db import (
+    get_tokens,
+    store_tokens,
+    update_tokens,
+    delete_tokens,
+    database,
+)
+
+
+@responses.activate
+@pytest.mark.asyncio
+async def test_get_tokens():
+    test_request_payload = "test123@gmail.com"
+    test_response_payload = {
+        "email": "test123@gmail.com",
+        "access_token": "foo",
+        "refresh_token": "bar",
+        "expires_at": 42,
+    }
+
+    responses.add(
+        responses.GET,
+        "https://training-song-api-koayon.vercel.app",
+        json=test_response_payload,
+        status=200,
+    )
+
+    # Mock the database object and its fetch_one method
+    database_mock = MagicMock()
+    database.fetch_one = AsyncMock(return_value=test_response_payload)
+
+    # Call the function with the test inputs
+    response = await get_tokens(test_request_payload)
+
+    # Verify the function returned the expected result
+    assert response == test_response_payload
+
+    # Verify fetch_one was called with the expected query
+    database.fetch_one.assert_called_once()
