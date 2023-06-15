a4cfb1507d3d8699e05dcf2f9efbc6221bce6036
koayon <58336584+koayon@users.noreply.github.com>
2023-06-15 18:56:58 +0100

    Add test coverage for db

--- tests/server/test_db.py
+++ tests/server/test_db.py
@@ -1,45 +1,99 @@
+import asyncio
 import json
-import responses
-
-import pytest
 from unittest.mock import AsyncMock, MagicMock
 
-from trainingsong.server.db import (
-    get_tokens,
-    store_tokens,
-    update_tokens,
-    delete_tokens,
-    database,
-)
+import pytest
+import responses
+from dotenv import load_dotenv
+
+from trainingsong.server import db
+
+load_dotenv()
 
 
-@responses.activate
+# @responses.activate
+# @pytest.mark.asyncio
+# async def test_get_tokens():
+#     test_request_payload = "test123@gmail.com"
+#     test_response_payload = {
+#         "email": "test123@gmail.com",
+#         "access_token": "foo",
+#         "refresh_token": "bar",
+#         "expires_at": 42,
+#     }
+
+#     responses.add(
+#         responses.GET,
+#         "https://training-song-api-koayon.vercel.app",
+#         json=test_response_payload,
+#         status=200,
+#     )
+
+#     db.database.fetch_one = AsyncMock(return_value=test_response_payload)
+#     response = await db.get_tokens(test_request_payload)
+
+#     assert response == test_response_payload
+#     db.database.fetch_one.assert_called_once()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_test_db():
+    db.create()
+    yield
+
+
 @pytest.mark.asyncio
-async def test_get_tokens():
-    test_request_payload = "test123@gmail.com"
-    test_response_payload = {
-        "email": "test123@gmail.com",
-        "access_token": "foo",
-        "refresh_token": "bar",
-        "expires_at": 42,
-    }
+async def test_store_tokens(setup_test_db):
+    email = "test@example.com"
+    access_token = "access_token"
+    refresh_token = "refresh_token"
+    expires_at = 1689727126
 
-    responses.add(
-        responses.GET,
-        "https://training-song-api-koayon.vercel.app",
-        json=test_response_payload,
-        status=200,
-    )
+    async with db.database_session():
+        await db.store_tokens(email, access_token, refresh_token, expires_at)
 
-    # Mock the database object and its fetch_one method
-    database_mock = MagicMock()
-    database.fetch_one = AsyncMock(return_value=test_response_payload)
+    # Assert tokens were stored
+    async with db.database_session():
+        result = await db.get_tokens(email)
+        if result:
+            assert result["email"] == email
+            assert result["access_token"] == access_token
+            assert result["refresh_token"] == refresh_token
+            assert result["expires_at"] == expires_at
+        else:
+            assert False
 
-    # Call the function with the test inputs
-    response = await get_tokens(test_request_payload)
 
-    # Verify the function returned the expected result
-    assert response == test_response_payload
+@pytest.mark.asyncio
+async def test_update_tokens(setup_test_db):
+    email = "test@example.com"
+    access_token = "new_access_token"
+    refresh_token = "new_refresh_token"
+    expires_at = 689727126
 
-    # Verify fetch_one was called with the expected query
-    database.fetch_one.assert_called_once()
+    async with db.database_session():
+        await db.update_tokens(email, access_token, refresh_token, expires_at)
+
+    # Assert tokens were updated
+    async with db.database_session():
+        result = await db.get_tokens(email)
+        if result:
+            assert result["email"] == email
+            assert result["access_token"] == access_token
+            assert result["refresh_token"] == refresh_token
+            assert result["expires_at"] == expires_at
+        else:
+            assert False
+
+
+@pytest.mark.asyncio
+async def test_delete_tokens(setup_test_db):
+    email = "test@example.com"
+
+    async with db.database_session():
+        await db.delete_tokens(email)
+
+    # Assert tokens were deleted
+    async with db.database_session():
+        result = await db.get_tokens(email)
+        assert result is None
